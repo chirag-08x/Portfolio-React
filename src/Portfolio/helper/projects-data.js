cb6a0e1dcfc9f0ca6e71bc936960853563e42e44
chirag-08x <cagrawal081200@gmail.com>
2022-04-13 12:37:27 +0530

    removed some projects

--- src/Portfolio/helper/projects-data.js
+++ src/Portfolio/helper/projects-data.js
@@ -8,25 +8,11 @@ const projects = [
     image: "./images/main-project-img/prjct-1.png",
   },
   {
-    name: "path finding visualizer",
-    path: `#`,
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eveniet amet odit aperiam, provident cum possimus sapiente minus quos! Ipsum?",
-    github: "#",
-    image: "./images/dummy.jpeg",
-  },
-  {
     name: "Comfy Sloth Store",
     path: "https://comfy-store08x.netlify.app/",
     text: "An online e-commerce where you can choose from variety of products and buy what's best suited for your house.",
     github: "https://github.com/chirag-08x/Comfy-Store",
     image: "./images/main-project-img/prjct-3.png",
-  },
-  {
-    name: "chatBiz",
-    path: "#",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eveniet amet odit aperiam, provident cum possimus sapiente minus quos! Ipsum?",
-    github: "#",
-    image: "./images/dummy.jpeg",
   },
   {
     name: "Portfolio",
