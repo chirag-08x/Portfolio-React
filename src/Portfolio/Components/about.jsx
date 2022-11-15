e966f28cb80e63fcba62c2ab5eab2bd9b9266378
chirag-08x <cagrawal081200@gmail.com>
2022-11-15 20:17:21 +0530

    about-me image updated

--- src/Portfolio/Components/about.jsx
+++ src/Portfolio/Components/about.jsx
@@ -6,17 +6,18 @@ const About = () => {
     <section className="section-center about">
       <article className="about-img-container">
         <figure className="about-hero-img">
-          <img src="./images/hero-main.png" alt="about" className="about-img" />
+          {/* <img src="./images/hero-main.png" alt="about" className="about-img" /> */}
+          <img src="./images/about4.jpg" alt="about" className="about-img" />
         </figure>
       </article>
       <article className="about-info-container">
         <h1 className="title">about</h1>
         <div className="underline"></div>
         <p className="info">
-          I am a 3<sup>rd</sup> year computer engineering student. I have been
-          learning and practicing frontend from quiet a long time now and got a
-          good hold of it. I love building websites and to work on new and
-          interesting projects.
+          I am a final year computer engineering student. I have been learning
+          and practicing frontend from quiet a long time now and got a good hold
+          of it. I love building websites and to work on new and interesting
+          projects.
         </p>
         <p className="info">
           I am really passionate about my work and love what I'm doing.I have a
